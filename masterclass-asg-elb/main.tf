518d5c9f7ac4303d6c22235ec9137763ba1acb26
Dmytro Batiievskyi <dmytro_batiievskyi@epam.com>
2017-11-26 21:51:22 +0200

    move data to separate file

--- masterclass-asg-elb/main.tf
+++ masterclass-asg-elb/main.tf
@@ -1,36 +1,3 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "all" {
-  vpc_id = "${data.aws_vpc.default.id}"
-}
-
-data "aws_security_group" "default" {
-  vpc_id = "${data.aws_vpc.default.id}"
-  name   = "tmp-ssh"
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-
-  filter {
-    name = "owner-alias"
-
-    values = [
-      "amazon",
-    ]
-  }
-}
-
 ######
 # Launch configuration and autoscaling group
 ######
