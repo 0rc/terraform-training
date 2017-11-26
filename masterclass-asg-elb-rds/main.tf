db85b57a3d75dc262d09dab0f4828af6b7d05762
Dmytro Batiievskyi <dmytro_batiievskyi@epam.com>
2017-11-26 15:04:58 +0200

    add a variant with rds

---
+++ masterclass-asg-elb-rds/main.tf
@@ -0,0 +1,163 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_security_group" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  name   = "tmp-ssh"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+######
+# Launch configuration and autoscaling group
+######
+module "autoscaling" {
+  source = "terraform-aws-modules/autoscaling/aws"
+
+  # Launch configuration
+  #
+  # Uncomment the value below and provide value of existing launch configuration to use it instead of making new one
+  # existing_launch_configuration = "aa"
+  lc_name = "example-lc"
+
+  image_id          = "${data.aws_ami.amazon_linux.id}"
+  instance_type     = "t2.micro"
+  security_groups   = ["${data.aws_security_group.default.id}"]
+  load_balancers    = ["${module.elb.this_elb_id}"]
+  target_group_arns = []
+
+  ebs_block_device = [
+    {
+      device_name           = "/dev/xvdz"
+      volume_type           = "gp2"
+      volume_size           = "8"
+      delete_on_termination = true
+    },
+  ]
+
+  root_block_device = [
+    {
+      volume_size = "8"
+      volume_type = "gp2"
+    },
+  ]
+
+  # Auto scaling group
+  asg_name                  = "example-asg"
+  vpc_zone_identifier       = ["${data.aws_subnet_ids.all.ids}"]
+  health_check_type         = "EC2"
+  min_size                  = 0
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "dev"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = "megasecret"
+      propagate_at_launch = true
+    },
+  ]
+}
+
+######
+# ELB
+######
+module "elb" {
+  source = "terraform-aws-modules/elb/aws"
+
+  name = "elb-example"
+
+  subnets         = ["${data.aws_subnet_ids.all.ids}"]
+  security_groups = ["${data.aws_security_group.default.id}"]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = "80"
+      instance_protocol = "HTTP"
+      lb_port           = "80"
+      lb_protocol       = "HTTP"
+    },
+  ]
+
+  health_check = [
+    {
+      target              = "HTTP:80/"
+      interval            = 30
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout             = 5
+    },
+  ]
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
+
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = "demodb"
+
+  engine            = "postgres"
+  engine_version    = "9.6.3"
+  instance_class    = "db.t2.micro"
+  allocated_storage = 5
+  storage_encrypted = false
+
+  name = "demodb"
+
+  username = "demouser"
+
+  password = "YourPwdShouldBeLongAndSecure!"
+  port     = "5432"
+
+  vpc_security_group_ids = ["${data.aws_security_group.default.id}"]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  backup_retention_period = 0
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  subnet_ids = ["${data.aws_subnet_ids.all.ids}"]
+
+  family = "postgres9.6"
+
+  final_snapshot_identifier = "demodb"
+}
